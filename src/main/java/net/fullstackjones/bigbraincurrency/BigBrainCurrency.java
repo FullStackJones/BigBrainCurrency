76c30649785f8a0e75d0bfea2a695979fabf751b
FullStackJones <gregoryjones0498@gmail.com>
2024-10-18 17:26:16 +0100

    feat: add recipes for mod blocks and items (#6)
    * recipes
    
    * remove unneeded

--- src/main/java/net/fullstackjones/bigbraincurrency/BigBrainCurrency.java
+++ src/main/java/net/fullstackjones/bigbraincurrency/BigBrainCurrency.java
@@ -13,6 +13,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
